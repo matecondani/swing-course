3f34853d6bdc77447319985ab1638b4b02083f17
Marcos Daniel Saguier <matecondani@gmail.com>
2023-10-20 00:01:46 -0300

    class 11

--- src/main/java/src/FormPanel.java
+++ src/main/java/src/FormPanel.java
@@ -6,11 +6,24 @@ import javax.swing.border.Border;
 
 public class FormPanel extends JPanel {
 
+    private JLabel nameLabel;
+    private JLabel occupationLabel;
+    private JTextField nameField;
+    private JTextField occupationField;
+    private JButton okBtn;
+
     public FormPanel() {
         Dimension dim = getPreferredSize();
         System.out.println(dim);
         dim.width = 250;
         setPreferredSize(dim);
+
+        nameLabel = new JLabel();
+        occupationLabel  = new JLabel();
+        nameField = new JTextField();
+        occupationField = new JTextField(10);
+
+        okBtn = new JButton("OK");
 
         Border innerBorder = BorderFactory.createTitledBorder("Add Person");
         Border outerBorder = BorderFactory.createEmptyBorder(5, 5, 5, 5);
