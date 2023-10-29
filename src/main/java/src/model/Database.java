ee8891fb537f5a8699a679a4c7da59e2986826cf
Marcos Daniel Saguier <matecondani@gmail.com>
2023-10-29 20:55:55 -0300

    class 38

--- src/main/java/src/model/Database.java
+++ src/main/java/src/model/Database.java
@@ -6,6 +6,11 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
@@ -14,6 +19,7 @@ import java.util.List;
 public class Database {
 
     private List<Person> people;
+    private Connection con;
 
     public Database() {
         this.people = new LinkedList<Person>();
@@ -48,6 +54,43 @@ public class Database {
             throw new RuntimeException(e);
         }
         ois.close();
+    }
+
+    public void connect() throws Exception {
+        if (con != null) return;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new Exception("Driver not found");
+        }
+        String url = "jdbc:mysql://localhost:3306/swingtest";
+        con = DriverManager.getConnection(url, "swingroot", "clavesecreta");
+
+        System.out.println("Connected: " + con);
+    }
+
+    public void disconnect() {
+        if (con != null) {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                System.out.println("cant close connection");
+            }
+        }
+    }
+
+    public void save() throws SQLException {
+        String checkSql = "select count(*) as count from people where id =?";
+        PreparedStatement checkStatement = con.prepareStatement(checkSql);
+        for (Person person : people) {
+            int id = person.getId();
+            checkStatement.setInt(1, id);
+            ResultSet checkResult = checkStatement.executeQuery();
+            checkResult.next();
+            int count = checkResult.getInt(1);
+            System.out.println("count for person with id " + id + " is " + count);
+        }
     }
 
     public void removePerson(int index) {
---
+++ src/main/java/src/testDB/TestDatabase.java
@@ -0,0 +1,32 @@
+package src.testDB;
+
+import java.sql.SQLException;
+import javax.xml.crypto.Data;
+import src.model.AgeCategory;
+import src.model.Database;
+import src.model.EmploymentCategory;
+import src.model.Gender;
+import src.model.Person;
+
+public class TestDatabase {
+
+    public static void main(String[] args) {
+        System.out.println("Running database");
+        Database db = new Database();
+        try {
+            db.connect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        db.addPerson(new Person("joe", "builder", AgeCategory.ADULT, EmploymentCategory.employed,"777",true, Gender.MALE));
+        db.addPerson(new Person("sue", "artist", AgeCategory.SENIOR, EmploymentCategory.selfEmployed,null,true, Gender.FEMALE));
+
+        try {
+            db.save();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        db.disconnect();
+    }
+}
